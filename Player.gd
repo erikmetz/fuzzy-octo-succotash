c5f123035bf942f9c25638763f8d158845a45ded
Erik Metz <erikmetz@Eriks-MacBook-Air.local>
2018-11-17 16:54:55 -0500

    nothing personal kid

---
+++ Player.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+export (int) var speed
+var screensize
+
+func _ready():
+	screensize = get_viewport_rect().size
+
+func _process(delta):
+	var velocity = Vector2()
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
